8117088b5bb124dc5599d42cca90b564b74e0df3
mramiro <manuel.ramiro.zabaleta@gmail.com>
2021-07-30 11:55:40 +0200

    Creación del archivo html, final del codigo del cuadrado

---
+++ figuras.js
@@ -0,0 +1,9 @@
+// Codigo del cuadrado
+ const ladoCuadrado = 5;
+ console.log("Los lados del cuadrado mide: " + ladoCuadrado + "cm");
+
+ const perimetroCuadrado = ladoCuadrado * 4;
+console.log("El perimetro del cuadrado es: " + perimetroCuadrado + "cm");
+
+const areaCuadrado =  ladoCuadrado * ladoCuadrado;
+console.log("El area del cuadrado es: " + areaCuadrado + "cm");
