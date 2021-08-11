1ea20ed7d7d2958199ec9f52c3fbd5a4aab0e067
mramiro <manuel.ramiro.zabaleta@gmail.com>
2021-08-11 13:28:00 +0200

    Terminado el JS del taller 2

--- descuentos.js
+++ descuentos.js
@@ -1,5 +1,18 @@
-
 function calcularPrecio(precio, descuento){
     var precioFinal = (precio*(100 - descuento))/100;
     return precioFinal
+}
+
+function clickDiscount(){
+    const inputPrice = document.getElementById("inputPrice");
+    const priceValue = inputPrice.value;
+
+    const inputDiscount = document.getElementById("inputDiscount");
+    const discountValue = inputDiscount.value;
+
+    const precioConDescuento = calcularPrecio(priceValue, discountValue);
+
+    const resultP = document.getElementById("resultP");
+    resultP.innerText = "El precio con descuento son: " + precioConDescuento;
+
 }
